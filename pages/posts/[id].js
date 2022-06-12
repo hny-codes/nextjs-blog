2f4584d21557728bcf5fb4f35dd51ea01cbad429
Henry <hnguyhenry@protonmail.com>
2022-06-12 14:44:16 -0700

    Refactor code with Prettier

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -2,13 +2,13 @@ import Layout from "../../components/layout";
 import { getAllPostsIds } from "../../lib/posts";
 
 export default function Post() {
-    return <Layout></Layout>
+  return <Layout></Layout>;
 }
 
 export async function getStaticPaths() {
-    const paths = getAllPostsIds();
-    return {
-        paths, 
-        fallback: false,
-    };
+  const paths = getAllPostsIds();
+  return {
+    paths,
+    fallback: false,
+  };
 }
