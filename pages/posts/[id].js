d6a1b840941f012c93a2f958e20d5129794d122a
Henry <hnguyhenry@protonmail.com>
2022-06-12 15:03:06 -0700

    Update Post component with Head tag

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,9 +1,13 @@
 import Layout from "../../components/layout";
+import Head from "next/head";
 import { getAllPostsIds, getPostData } from "../../lib/posts";
 
 export default function Post({ postData }) {
   return (
     <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
       {postData.title}
       <br />
       {postData.id}
