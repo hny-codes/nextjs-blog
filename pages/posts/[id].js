010a0b64ca0747ccf56d708f7b6eaa2b0c96ad24
Henry <hnguyhenry@protonmail.com>
2022-06-12 14:57:57 -0700

    Update getStaticProps to use await

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -20,7 +20,7 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id);
+  const postData = await getPostData(params.id);
   return {
     props: {
       postData,
